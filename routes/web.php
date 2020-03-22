89a537b83c8e87cf5caea4c56d8b5c43e1efaee7
Rodrigo Aggeu Lopes de Medeiros <32340350+rodrigoaggeu@users.noreply.github.com>
2020-03-22 10:00:56 -0300

    update lingaugem pt-br

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
+| ------------------------------------------------- -------------------------
+| Rotas da Web
+| ------------------------------------------------- -------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| Aqui é onde você pode registrar rotas da web para seu aplicativo. Estes
+| rotas são carregadas pelo RouteServiceProvider dentro de um grupo que
+| contém o grupo de middleware "web". Agora crie algo ótimo!
 |
 */
 
